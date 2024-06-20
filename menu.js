23f1f0dddedb7cc424b4d26501491e76016b984d
WagJ7 <wagnerjsilvestre@hotmail.com>
2024-06-19 21:07:40 -0300

    first commit

---
+++ menu.js
@@ -0,0 +1,29 @@
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const menuItems = document.querySelectorAll('.menu-item');
+
+    menuItems.forEach(item => {
+        item.addEventListener('click', function() {
+            console.log('Item clicked:', this); // Depuração
+
+            // Toggle active class for the clicked item
+            this.classList.toggle('active');
+
+            // Get the next sibling element, which should be the .menu-content div
+            const content = this.nextElementSibling;
+            console.log('Content element:', content); // Depuração
+
+            // Check if the content is currently displayed
+            if (content.style.maxHeight) {
+                // If it is, hide it
+                content.style.maxHeight = null;
+                console.log('Content hidden'); // Depuração
+            } else {
+                // Otherwise, show it
+                content.style.maxHeight = content.scrollHeight + "px";
+                console.log('Content shown'); // Depuração
+            }
+        });
+    });
+});
